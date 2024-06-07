6c67a9ba01b26b97396791366a147ba2fb64b80b
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-08 01:28:03 +0545

    wildcharacter

---
+++ Learning/character.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main()
+{
+    // char name = 97;
+    // std::cout <<name<<std::endl;
+
+   // std::cout<<int('a')<<std::endl;
+
+   //std::cout<<char(89)<<char(97)<<std::endl;
+
+    return 0;
+}
---
+++ Learning/wildcharacter.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    wchar_t ch = L'Ă';
+    wcout<<int(ch)<<endl;
+    return 0;
+}

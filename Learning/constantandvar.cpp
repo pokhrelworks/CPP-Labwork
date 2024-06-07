e9bfc898012f0b33937fafe60d7212e256ef7611
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-08 00:00:05 +0545

    new

---
+++ Learning/constantandvar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    const int a = 10;
+    const std::string name = "Ankit";
+    std::cout << a << std::endl;
+    std::cout << "My name is " << name << std::endl;
+    int b = 10;
+    std::cout << b << std::endl;
+    return 0;
+}
